206ef21bbc5921c6ca4688a683ef7ac33ec0d064
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-05 14:41:52 +0900

    user詳細機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
   def tops
   end
 
@@ -33,5 +35,8 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
 
 end
